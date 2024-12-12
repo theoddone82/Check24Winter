639f0c447127a1606b5b4ddcd3a58353b71c83ea
studseelch2803 <christian.seeleitner@stud.th-rosenheim.de>
2024-12-13 00:37:04 +0100

    performance

--- attemptNumberTwo/performance_monitor/views.py
+++ attemptNumberTwo/performance_monitor/views.py
@@ -1,17 +1,21 @@
+from django.shortcuts import render
 import psutil
-from django.http import JsonResponse
 import subprocess
+import os
 
-def get_metrics(request):
+def performance_dashboard(request):
+    # Generate the current system metrics
     metrics = {
         'cpu': psutil.cpu_percent(interval=1),
         'memory': psutil.virtual_memory()._asdict(),
         'disk': psutil.disk_usage('/')._asdict(),
     }
-    return JsonResponse(metrics)
 
-def get_htop_screenshot(request):
-    # Save the htop screenshot to a temporary file
+    # Generate the htop screenshot
     output_file = "/tmp/htop_screenshot.png"
     subprocess.run(['htop', '-C', '--no-color', '-d', '1'], stdout=open(output_file, 'wb'))
-    return JsonResponse({'image_path': output_file})
+
+    return render(request, 'performance_monitor/dashboard.html', {
+        'metrics': metrics,
+        'htop_image': '/performance/static/htop_screenshot.png'  # Static path for the image
+    })
