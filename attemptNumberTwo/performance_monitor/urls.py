641814754ba962cb1f28f84904f3299078514b20
studseelch2803 <christian.seeleitner@stud.th-rosenheim.de>
2024-12-13 21:16:38 +0100

    test view

--- attemptNumberTwo/performance_monitor/urls.py
+++ attemptNumberTwo/performance_monitor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('metrics/', views.get_metrics, name='get_metrics'),
-    path('htop-screenshot/', views.get_htop_screenshot, name='get_htop_screenshot'),
+    # path('metrics/', views.get_metrics, name='get_metrics'),
+    # path('htop-screenshot/', views.get_htop_screenshot, name='get_htop_screenshot'),
     path('dashboard/', views.performance_dashboard, name='performance_dashboard'),
 ]
